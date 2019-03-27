d1adc48a1cc72ad05ebc8edccb126b5291d593df
charge <ahujaarush@gmail.com>
2019-03-28 02:22:02 +0530

    add frontend build, update compose, update nginx conf

---
+++ docs/build/precache-manifest.3d686dd9b52653ec66fbb4da6f3a2297.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7d9a339a7386d1a7f2fa",
+    "url": "/static/js/main.b4860582.chunk.js"
+  },
+  {
+    "revision": "ee76d749184c381cef78",
+    "url": "/static/js/2.1482f2f5.chunk.js"
+  },
+  {
+    "revision": "7d9a339a7386d1a7f2fa",
+    "url": "/static/css/main.963ba1f5.chunk.css"
+  },
+  {
+    "revision": "03f6f14b5cbc096866c6f9f4c2e8a1eb",
+    "url": "/index.html"
+  }
+];
