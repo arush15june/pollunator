f589ef811411057efada2a508cdd8a3c01dfc89d
charge <ahujaarush@gmail.com>
2019-03-27 23:45:44 +0530

    update populator

--- app/populate.py
+++ app/populate.py
@@ -1,7 +1,9 @@
 from database import init_db
 import models
+from wrapper import PollutionAPIWrapper
 
 init_db()
+api_handler = PollutionAPIWrapper()
 
 if models.Station.query.count() == 0:
         print('** Populating Stations')
